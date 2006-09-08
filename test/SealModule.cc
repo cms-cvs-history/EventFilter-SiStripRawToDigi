401434dbf1d999d3166ed449527fd099d22723ef
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-09-08 15:18:38 +0000

    tidy up of cfi, cff and cfg files, and moved some source to stubs

--- test/SealModule.cc
+++
@@ -1,7 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-DEFINE_SEAL_MODULE();
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "EventFilter/SiStripRawToDigi/test/SiStripAnalyzeDigis.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripAnalyzeDigis)
-
