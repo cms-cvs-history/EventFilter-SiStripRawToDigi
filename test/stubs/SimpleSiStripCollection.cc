1a3422b4b5c4870ee325abf6bdd90cc12a30ac10
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-12 19:35:19 +0000

    SimpleSiStripCluster collection

---
+++ test/stubs/SimpleSiStripCollection.cc
@@ -0,0 +1,12 @@
+#include "EventFilter/SiStripRawToDigi/test/stubs/SimpleSiStripCollection.h"
+
+using namespace std;
+
+SimpleSiStripCollection::SimpleSiStripCollection(string label) : 
+
+  clusters_(),
+  label_(label)
+
+{;}
+
+SimpleSiStripCollection::~SimpleSiStripCollection() {;}
