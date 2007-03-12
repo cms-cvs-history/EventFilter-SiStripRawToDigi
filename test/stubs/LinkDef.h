cff19c480f4dfa0af4b8368bbed195d8e828599b
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-12 19:29:40 +0000

    for SimpleCollection and SimpleSiStripCluster classes

---
+++ test/stubs/LinkDef.h
@@ -0,0 +1,13 @@
+#include "EventFilter/SiStripRawToDigi/test/stubs/SimpleSiStripCluster.h"
+#include "EventFilter/SiStripRawToDigi/test/stubs/SimpleSiStripCollection.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class SimpleSiStripCluster+;
+#pragma link C++ class SimpleSiStripCollection+;
+
+#endif
