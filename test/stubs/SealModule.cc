9e475d35b01404877d265a12fba638621a9ea316
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-12 19:30:11 +0000

    Added SiStripPerformanceRecordModule

--- test/stubs/SealModule.cc
+++ test/stubs/SealModule.cc
@@ -1,7 +1,12 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
 #include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
 DEFINE_SEAL_MODULE();
 
 #include "EventFilter/SiStripRawToDigi/test/stubs/AnalyzeSiStripDigis.h"
 DEFINE_ANOTHER_FWK_MODULE(AnalyzeSiStripDigis);
+
+#include "EventFilter/SiStripRawToDigi/test/stubs/SiStripPerformanceRecordModule.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripPerformanceRecordModule);
+
 
