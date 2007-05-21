c0c1706ac4c8b7458cdd3c103edb4b1c483a80a7
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-05-21 21:21:13 +0000

    cmsRun's up to 2 config files on lxbatch copying output.txt to local dir and root files to castor

---
+++ test/data/rawtoclusters.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+### Run using the following syntax:
+### bsub -q 8nm -m lxb7350.cern.ch rawtoclusters.sh $FILE1 $FILE2
+### Output root file copied to castor, txt file to local config dir.
+
+#arguments
+FILE1_=$1;
+FILE2_=$2;
+
+#hardcoded directories
+CFGDIR=~pwing/scratch0/CMSSW_1_3_1_HLT2/src/EventFilter/SiStripRawToDigi/data;
+CASTORFILE=/castor/cern.ch/user/p/pwing/LowLevelRecon
+AUTHDIR=~pwing/public/scripts/RawToDigi/post_140
+
+#record current directory
+WNDIR=$(pwd);
+
+#set environment
+cd $CFGDIR;
+source /afs/cern.ch/cms/sw/cmsset_default.sh;
+eval `scramv1 runtime -sh`;
+cd $WNDIR;
+
+#copy over necessary files
+touch $CFGDIR/output.txt
+cp $CFGDIR/output.txt .
+cp $AUTHDIR/authentication.xml .
+
+#run
+cmsRun $CFGDIR'/'$FILE1_;
+if [ "$FILE2" != "" ];then
+    cmsRun $CFGDIR'/'$FILE2_;
+fi
+
+#store output files
+rfcp *.root $CASTORFILE;
+cp output.txt $CFGDIR
