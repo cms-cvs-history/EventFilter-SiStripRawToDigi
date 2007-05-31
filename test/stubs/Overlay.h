9438582f7028d50367f08ae36edc32861848c179
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-05-31 15:40:44 +0000

    tidy/debug

--- test/stubs/Overlay.h
+++ test/stubs/Overlay.h
@@ -12,20 +12,17 @@
 #include <iostream>
 
 class Overlay {
-
+  
  public:
 
   Overlay(): canvas_(0), legend_(0), labOne_(""), labTwo_(""), labThree_(""), labFour_(""), labFive_("") {
-
-    canvas_ = new TCanvas("", "",200,10,700,500);
-    legend_ = new TLegend(0.7,0.56,0.9,0.69);
-    legend_->SetFillColor(kWhite);
-  }
+    
+      canvas_ = new TCanvas("", "",200,10,700,500);
+      legend_ = new TLegend(0.7,0.56,0.9,0.69);
+      legend_->SetFillColor(kWhite);
+    }
   
-  ~Overlay() {
-    delete canvas_;
-    delete legend_;
-  }
+  ~Overlay() { delete canvas_; delete legend_; }
 
   void label(std::string labOne="", std::string labTwo="", std::string labThree="", std::string labFour="", std::string labFive="") {
     labOne_ = labOne;
@@ -37,35 +34,31 @@ class Overlay {
   
   void graphs(TGraph* const one, TGraph* const two=0, TGraph* const three=0, TGraph* const four=0,TGraph* const five=0, bool legend=false, string option="") {
     
-  canvas_->Clear();
-  legend_->Clear();
-  //canvas_->SetGrid();
-  canvas_->SetLogx(0);
-  canvas_->SetLogy(0);
-
-  //Format graph and build legend
-  if (one) {
-    one->SetMarkerColor(kBlue);
+    if (!one) return;
+    reset();
+    
+    //Draw graph and build legend
+    one->SetMarkerColor(onecolor);
     one->Draw(("AP"+option).c_str());
-    if (legend) legend_->AddEntry(one,labOne_.c_str(),"p");}
-
+    if (legend) legend_->AddEntry(one,labOne_.c_str(),"p");
+  
   if (two) {
-    two->SetMarkerColor(kRed);
+    two->SetMarkerColor(twocolor);
     two->Draw(("P"+option).c_str());
     if (legend) legend_->AddEntry(two,labTwo_.c_str(),"p");}
 
   if (three) {
-    three->SetMarkerColor(kBlack);
+    three->SetMarkerColor(threecolor);
     three->Draw(("P"+option).c_str());
     if (legend) legend_->AddEntry(three,labThree_.c_str(),"p");}
 
   if (four) {
-    four->SetMarkerColor(kGreen);
+    four->SetMarkerColor(fourcolor);
     four->Draw(("P"+option).c_str());
     if (legend) legend_->AddEntry(four,labThree_.c_str(),"p");}
 
   if (five) {
-    five->SetMarkerColor(kMagenta);
+    five->SetMarkerColor(fivecolor);
     five->Draw(("P"+option).c_str());
     if (legend) legend_->AddEntry(five,labThree_.c_str(),"p");}  
   
@@ -75,69 +68,57 @@ class Overlay {
 
 void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", string ytitle="", string ztitle="",string option="") {
 
-  canvas_->Clear();
-  legend_->Clear();
-  //canvas_->SetGrid();
-  canvas_->SetLogx(0);
-  canvas_->SetLogy(0);
-
-  if (graph) {
-    graph->Draw(option.c_str());
-    graph->SetTitle(title.c_str());
-    graph->GetXaxis()->SetTitle(xtitle.c_str());
-    graph->GetYaxis()->SetTitle(ytitle.c_str());
-    graph->GetZaxis()->SetTitle(ztitle.c_str());
-  }
-
+  if (!graph) return;
+  reset();
+  
+  graph->Draw(option.c_str());
+  graph->SetTitle(title.c_str());
+  graph->GetXaxis()->SetTitle(xtitle.c_str());
+  graph->GetYaxis()->SetTitle(ytitle.c_str());
+  graph->GetZaxis()->SetTitle(ztitle.c_str());
 }
  
  void histos(TH1* const one, TH1* const two=0, TH1* const three=0, TH1* const four=0, TH1* const five=0, bool legend=false, string option="") {
 
-  canvas_->Clear();
-  legend_->Clear();
-  //canvas_->SetGrid();
-  canvas_->SetLogx(0);
-  canvas_->SetLogy(0);
+   if (!one) return;
+   reset();
 
   //Format histo and build legend
-  if (one) {
-    one->SetLineColor(kRed);
-    one->SetMarkerColor(kRed);
-    if (legend) legend_->AddEntry(one,labOne_.c_str(),"l");}
+    one->SetLineColor(onecolor);
+    one->SetMarkerColor(onecolor);
+    if (legend) legend_->AddEntry(one,labOne_.c_str(),"l");
 
   if (two) {
-    two->SetLineColor(kBlue);
-    two->SetMarkerColor(kBlue);
+    two->SetLineColor(twocolor);
+    two->SetMarkerColor(twocolor);
     if (legend) legend_->AddEntry(two,labTwo_.c_str(),"l");}
 
   if (three) {
-    three->SetLineColor(kBlack);
-    three->SetMarkerColor(kBlack);
+    three->SetLineColor(threecolor);
+    three->SetMarkerColor(threecolor);
     if (legend) legend_->AddEntry(three,labThree_.c_str(),"l");}
 
   if (four) {
-    four->SetLineColor(kGreen);
-    four->SetMarkerColor(kGreen);
+    four->SetLineColor(fourcolor);
+    four->SetMarkerColor(fourcolor);
     if (legend) legend_->AddEntry(four,labFour_.c_str(),"l");}
 
   if (five) {
-    five->SetLineColor(kMagenta);
-    five->SetMarkerColor(kMagenta);
+    five->SetLineColor(fivecolor);
+    five->SetMarkerColor(fivecolor);
     if (legend) legend_->AddEntry(five,labFive_.c_str(),"l");}
 
   //Draw
-  Int_t maxone = 0, maxtwo = 0, maxthree = 0, maxfour = 0, maxfive = 0;
-  if (one) maxone = one->GetMaximumBin();
-  if (two) maxtwo = two->GetMaximumBin();
-  if (three) maxthree = three->GetMaximumBin();
-  if (four) maxfour = four->GetMaximumBin();
-  if (five) maxfive = five->GetMaximumBin();
-
-  Int_t yrange[] = {maxone,maxtwo,maxthree,maxfour,maxfive};
-  Int_t maxy = max_element(yrange,yrange+5)-yrange;
+  std::vector<Double_t> max(5,0);
+  max[0] = one->GetMaximum();
+  if (two) max[1] = two->GetMaximum();
+  if (three) max[2] = three->GetMaximum();
+  if (four) max[3] = four->GetMaximum();
+  if (five) max[4] = five->GetMaximum();
+  Int_t maxy = max_element(max.begin(),max.end())-max.begin();
 
   if (maxy == 0) {
-    if (one) one->Draw(option.c_str());
+    one->Draw(option.c_str());
     if (two) two->Draw(("SAME"+option).c_str());
     if (three) three->Draw(("SAME"+option).c_str());
     if (four) four->Draw(("SAME"+option).c_str());
@@ -146,7 +127,7 @@ void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", 
 
   else if (maxy == 1) {
     two->Draw(option.c_str());
-    if (one) one->Draw(("SAME"+option).c_str());
+    one->Draw(("SAME"+option).c_str());
     if (three) three->Draw(("SAME"+option).c_str());
     if (four) four->Draw(("SAME"+option).c_str());
     if (five) five->Draw(("SAME"+option).c_str());
@@ -154,7 +135,7 @@ void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", 
 
   else if (maxy == 2) {
     three->Draw(option.c_str());
-    if (one) one->Draw(("SAME"+option).c_str());
+    one->Draw(("SAME"+option).c_str());
     if (two) two->Draw(("SAME"+option).c_str());
     if (four) four->Draw(("SAME"+option).c_str());
     if (five) five->Draw(("SAME"+option).c_str());
@@ -162,7 +143,7 @@ void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", 
 
   else if (maxy == 3) {
     four->Draw(option.c_str());
-    if (one) one->Draw(("SAME"+option).c_str());
+    one->Draw(("SAME"+option).c_str());
     if (two) two->Draw(("SAME"+option).c_str());
     if (three) three->Draw(("SAME"+option).c_str());
     if (five) five->Draw(("SAME"+option).c_str());
@@ -170,7 +151,7 @@ void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", 
 
   else if (maxy == 4) {
     five->Draw(option.c_str());
-    if (one) one->Draw(("SAME"+option).c_str());
+    one->Draw(("SAME"+option).c_str());
     if (two) two->Draw(("SAME"+option).c_str());
     if (three) three->Draw(("SAME"+option).c_str());
     if (four) four->Draw(("SAME"+option).c_str());
@@ -178,16 +159,15 @@ void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", 
 
   //Draw legend
   if (legend) legend_->Draw();
-}
+ }
  
- void marker(Double_t x,Double_t y) {
+ TMarker* marker(Double_t x,Double_t y,Int_t mark) {
    
    //NB memory leak here....
-   TMarker* marker = new TMarker(x,y,24);
-   marker->SetMarkerColor(kGreen);
-   marker->SetMarkerSize(2.);
+   TMarker* marker = new TMarker(x,y,mark);
    canvas_->cd();
    marker->Draw();
+   return marker;
  }
  
  void save(string name) {
@@ -200,12 +180,26 @@ void Overlay::graphs2D(TGraph2D* const graph,string title="", string xtitle="", 
 
  private:
 
-  TLegend* legend_;
-  std::string labOne_;
-  std::string labTwo_;
-  std::string labThree_;
-  std::string labFour_;
-  std::string labFive_;
+ void reset() {
+
+   canvas_->Clear();
+   legend_->Clear();
+   canvas_->SetLogx(0);
+   canvas_->SetLogy(0);
+ }
+
+ static const Int_t onecolor = 1; 
+ static const Int_t twocolor = 2;
+ static const Int_t threecolor = 104;
+ static const Int_t fourcolor = 106; 
+ static const Int_t fivecolor = 108;
+ 
+ TLegend* legend_;
+ std::string labOne_;
+ std::string labTwo_;
+ std::string labThree_;
+ std::string labFour_;
+ std::string labFive_;
 };
 
 #endif
