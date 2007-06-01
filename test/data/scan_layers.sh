0ee5b5ebb1f35e99e3d7f4d739fb9f5012fe0f4e
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-06-01 15:56:30 +0000

    bug fixes

--- test/data/scan_layers.sh
+++ test/data/scan_layers.sh
@@ -10,6 +10,7 @@ WNDIR=$(pwd);
 
 #config directories
 DATA=$1/src/EventFilter/SiStripRawToDigi/data;
+TESTDATA=$1/src/EventFilter/SiStripRawToDigi/test/data;
 CONN=$1/src/CalibTracker/SiStripConnectivity/data/SiStripRegionConnectivity.cfi;
 
 #set environment
@@ -21,23 +22,35 @@ cd $WNDIR;
 #copy over necessary files
 cp /afs/cern.ch/cms/cmt/onlinedev/data/cabling/131/authentication.xml .
 
-#useful variables
-replacefull='replace SiStripRegionConnectivity.SiStripFull.'
-tib=$replacefull'TIBLayers = {1,2,3,4};'
-tob=$replacefull'TOBLayers = {1,2,3,4,5,6};'
-tec=$replacefull'TECLayers = {1,2,3,4,5,6,7,8,9};'
-tid=$replacefull'TIDLayers = {1,2,3};'
+#useful values
+replace='replace SiStripRegionConnectivity.'
+empty=$replace'TIBLayers={};'$replace'TOBLayers={};'$replace'TECLayers={};'$replace'TIDLayers={};'
+namefield='TreeName'
+equals=' = '
+string='"'
+endline=' #'
+
+#available settings
+tib=$replace'TIBLayers={1,2,3,4};'
+tob=$replace'TOBLayers={1,2,3,4,5,6};'
+tec=$replace'TECLayers={1,2,3,4,5,6,7,8,9};'
+tid=$replace'TIDLayers={1,2,3};'
+tobouter=$replace'TOBLayers={4,5,6};'
+tecouter=$replace'TECLayers={7,8,9};'
 
 #run
-cp $CONN conn.txt
-for layers in $tob $tob$tec $tib$tid $tib$tid$tob $tib$tid$tob$tec
+cp $CONN conn.txt;
+for layers in "$tob$tec" "$tib$tid" "$tib$tid$tob$tec" #$tobouter$tecouter $tib$tid$tobouter$tecouter 
 do
 
-sed '$a'"$layers" < $CONN > tmp.cfg; mv tmp.txt $CONN;
+sed '$a'"$empty" < $CONN > tmp.txt;
+sed '$a'"$layers" < tmp.txt > $CONN;
+name=$string${layers//$replacefull}$string;
+replace "$namefield" "$namefield$equals$name$endline" -- $TESTDATA/SiStripPerformanceRecordModule.cfi;
 cmsRun $DATA/RegionalReconstruction.cfg;
-cp conn.txt $CONN
+cp conn.txt $CONN;
 
 done
 
 #save root files
-rfcp *.root $2
+rfcp *.root $2;
