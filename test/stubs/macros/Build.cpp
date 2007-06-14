471a982584b9c9f1b85115249430174cd81b6dbf
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-06-14 19:05:24 +0000

    removed dependency on CMSSW libs

--- test/stubs/macros/Build.cpp
+++ test/stubs/macros/Build.cpp
@@ -3,7 +3,6 @@
 gROOT->Reset("a");
 gSystem->AddIncludePath("-I/$CMSSW_BASE/src/");
 gSystem->AddIncludePath("-I/$CMSSW_RELEASE_BASE/src/");
-gSystem->Load("$CMSSW_BASE/lib/slc4_ia32_gcc345/libtestEventFilterSiStripRawToDigi");
 gSystem->CompileMacro("PerformanceAnalysis.cc","fkg","libPerformanceAnalysis");
 gSystem->CompileMacro("PerformanceOverlay.cc","fkg","libPerformanceOverlay");
 
