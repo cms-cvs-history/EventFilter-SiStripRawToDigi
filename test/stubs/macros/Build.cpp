fb9e126a7b989c3f6c1d88e309aedd8ddf6b97e7
cmsbuild <>
2007-07-12 17:23:17 +0000

    This commit was manufactured by cvs2git to create branch 'EventFilter- SiStripRawToDigi-qa_branch_V03-00-00-06'.
    Sprout from master 2007-07-12 17:23:16 UTC pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch> 'define roi using SiStripRegionCabling util methods'
    Delete:
        BuildFile
        data/AnalyzeSiStripDigis.cfg
        data/DBCabling.cfi
        data/DBNoise.cfi
        data/Digi2Raw2Clusters.cfg
        data/Digi2Raw2Digi2Clusters.cfg
        data/DigiToRaw.cfi
        data/FedChannelDigis.cfi
        data/Geometry.cff
        data/PoolSource.cfi
        data/ReadExampleRUFileAndCreateFedChannelDigis.cfg
        data/ReadExampleRUFileAndCreateSiStripDigis.cfg
        data/RegionalDigi2Raw2RecHits.cfg
        data/RegionalReconstruction.cfg
        data/SiStripDigis.cfi
        data/SiStripRawToClusters.cff
        data/SiStripRawToClusters.cfi
        data/SiStripRawToClustersModule.cfi
        data/SiStripRawToClustersRoI.cfi
        data/TrivialDigi2Raw2Clusters.cfg
        data/TrivialDigi2Raw2Digi2Clusters.cfg
        data/TrivialRegionalDigi2Raw2Clusters.cfg
        doc/SiStripRawToDigi.doc
        doc/html/index.html
        doc/html/overview.html
        interface/SiStripDigiToRaw.h
        interface/SiStripRawToClustersLazyUnpacker.h
        interface/SiStripRawToDigiUnpacker.h
        interface/TFHeaderDescription.h
        plugins/BuildFile
        plugins/SiStripDigiToRawModule.cc
        plugins/SiStripDigiToRawModule.h
        plugins/SiStripRawToClusters.cc
        plugins/SiStripRawToClusters.h
        plugins/SiStripRawToClustersModule.cc
        plugins/SiStripRawToClustersModule.h
        plugins/SiStripRawToClustersRoI.cc
        plugins/SiStripRawToDigiModule.cc
        plugins/SiStripRawToDigiModule.h
        plugins/module.cc
        src/SiStripDigiToRaw.cc
        src/SiStripRawToClustersLazyUnpacker.cc
        src/SiStripRawToDigiUnpacker.cc
        test/BuildFile
        test/data/SiStripClustersDSVBuilder.cfi
        test/data/SiStripPerformance.cfi
        test/data/SiStripRawToClustersDummyUnpacker.cfi
        test/data/SiStripTrivialClusterSource.cfi
        test/data/SiStripTrivialDigiSource.cfi
        test/data/batch.sh
        test/data/rawtoclusters.sh
        test/data/scan_dR.sh
        test/data/scan_layers.sh
        test/plugins/AnalyzeSiStripDigis.cc
        test/plugins/AnalyzeSiStripDigis.h
        test/plugins/SiStripClustersDSVBuilder.cc
        test/plugins/SiStripClustersDSVBuilder.h
        test/plugins/SiStripPerformance.cc
        test/plugins/SiStripPerformance.h
        test/plugins/SiStripRawToClustersDummyUnpacker.cc
        test/plugins/SiStripRawToClustersDummyUnpacker.h
        test/plugins/SiStripTrivialClusterSource.cc
        test/plugins/SiStripTrivialClusterSource.h
        test/plugins/SiStripTrivialDigiSource.cc
        test/plugins/SiStripTrivialDigiSource.h
        test/plugins/module.cc
        test/stubs/Constants.h
        test/stubs/Efficiency.h
        test/stubs/EventData.h
        test/stubs/LinkDef.h
        test/stubs/Overlay.h
        test/stubs/SiStripTrivialDigiAnalysis.cc
        test/stubs/SiStripTrivialDigiAnalysis.h
        test/stubs/SimpleElectron.h
        test/stubs/SimpleGenJet.h
        test/stubs/SimpleHCluster.h
        test/stubs/SimpleJet.h
        test/stubs/SimpleParticle.h
        test/stubs/SimpleSCluster.h
        test/stubs/SimpleSiStripCluster.h
        test/stubs/SimpleTrack.h
        test/stubs/SimpleTrigger.h
        test/stubs/Utility.h
        test/stubs/macros/Build.cpp
        test/stubs/macros/PerformanceAnalysis.cc
        test/stubs/macros/PerformanceAnalysis.cpp
        test/stubs/macros/PerformanceAnalysis.h
        test/stubs/macros/PerformanceOverlay.cc
        test/stubs/macros/PerformanceOverlay.h

--- test/stubs/macros/Build.cpp
+++
@@ -1,9 +0,0 @@
-{
-
-gROOT->Reset("a");
-gSystem->AddIncludePath("-I/$CMSSW_BASE/src/");
-gSystem->AddIncludePath("-I/$CMSSW_RELEASE_BASE/src/");
-gSystem->CompileMacro("PerformanceAnalysis.cc","fkg","libPerformanceAnalysis");
-gSystem->CompileMacro("PerformanceOverlay.cc","fkg","libPerformanceOverlay");
-
-}
