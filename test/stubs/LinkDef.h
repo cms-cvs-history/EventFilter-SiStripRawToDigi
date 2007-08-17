44644e290dd7e6f9c3e9ae0f467dca39ee697dfd
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-08-17 15:26:08 +0000

    uses new simpleanalysis classes

---
+++ test/stubs/LinkDef.h
@@ -0,0 +1,29 @@
+
+#include "EventFilter/SiStripRawToDigi/test/stubs/simpleanalysis/SimpleAnalysis.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class SimpleEfficiency+;
+#pragma link C++ class SimpleEfficiencyGraph+;
+#pragma link C++ class SimpleEfficiencyGraph2D+;
+#pragma link C++ class SimplePurityGraph+;
+#pragma link C++ class SimplePurityGraph2D+;
+#pragma link C++ class SimpleCorrelation+;
+#pragma link C++ class SimpleTrack+;
+#pragma link C++ class SimpleTrigger+;
+#pragma link C++ class SimpleGenParticle+;
+#pragma link C++ class SimpleMet+;
+#pragma link C++ class SimpleSCluster+;
+#pragma link C++ class SimpleHCluster+;
+#pragma link C++ class SimpleGenJet+;
+#pragma link C++ class SimpleJet+;
+#pragma link C++ class SimpleElectron+;
+#pragma link C++ class SimpleSiStripDigi+;
+#pragma link C++ class SimpleSiStripCluster+;
+#pragma link C++ class SimpleEventData+;
+
+#endif
