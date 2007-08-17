87fe81e14ead2df59a1568ffafb1e13917918f61
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-08-17 15:29:42 +0000

    minor

--- test/data/rawtoclusters.sh
+++ test/data/rawtoclusters.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 ### Run using the following syntax:
-### bsub -q 8nm -m lxb7350.cern.ch rawtoclusters.sh $1 $2 $3 $4
-### $1 = cfg file 1.
-### $2 = cfg file 2.
-### $3 = CMSSW_BASE.
-### $4 = output root file destination.
+### bsub -q 8nm -m lxb7350.cern.ch rawtoclusters.sh $1 $2 $3 $4 $5
+### $1 = CMSSW_BASE.
+### $2 = output root file destination.
+### $3 = cfg file 1.
+### $4 = cfg file 2.
+### $5 = cfg file 3.
 
-#record current directory
+# record current directory
 WNDIR=$(pwd);
 
-#config directories
-CFGDIR=$3/src/EventFilter/SiStripRawToDigi/data;
+# config directories
+CFGDIR=$1/src/EventFilter/SiStripRawToDigi/test;
 
-#set environment
+# set environment
 cd $CFGDIR;
 source /afs/cern.ch/cms/sw/cmsset_default.sh;
 eval `scramv1 runtime -sh`;
 cd $WNDIR;
 
-#copy over necessary files
+# copy over necessary files
 cp /afs/cern.ch/cms/cmt/onlinedev/data/cabling/131/authentication.xml .
 
-#run
-cmsRun $CFGDIR'/'$1;
-if [ "$2" != "" ];then
-    cmsRun $CFGDIR'/'$2;
+# run
+cmsRun $CFGDIR'/'$3;
+if [ "$4" != "" ];then
+    cmsRun $CFGDIR'/'$4;
+fi
+if [ "$5" != "" ];then
+    cmsRun $CFGDIR'/'$5;
 fi
 
-#store output files
-rfcp *.root $4;
+# store output files
+rfcp *.root $2;
