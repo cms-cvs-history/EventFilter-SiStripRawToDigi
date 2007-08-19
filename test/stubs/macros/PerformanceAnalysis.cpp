7f42fdcf9fb79eb27138b4e1958d635e1276ff2b
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-08-19 17:45:46 +0000

    tidy

--- test/stubs/macros/PerformanceAnalysis.cpp
+++ test/stubs/macros/PerformanceAnalysis.cpp
@@ -1,14 +1,14 @@
 {
 
-TFile* file = new TFile("file:/tmp/pwing/SiStripPerformance.root","UPDATE");
+TFile* file = new TFile("","UPDATE");
 
-PerformanceAnalysis one(file,"raw2digis");
+PerformanceAnalysis one(file,"");
 one.book(); one.format(); one.analyze(); one.save(); one.unbook();
-/*
-PerformanceAnalysis two(file,"raw2clusters");
+
+PerformanceAnalysis two(file,"");
 two.book(); two.format(); two.analyze(); two.save(); two.unbook();
 
-PerformanceAnalysis three(file,"raw2rechits"); 
+PerformanceAnalysis three(file,""); 
 three.book(); three.format(); three.analyze(); three.save(); three.unbook();
 
 PerformanceAnalysis four(file,"");
@@ -16,70 +16,18 @@ four.book(); four.format(); four.analyze(); four.save(); four.unbook();
 
 PerformanceAnalysis five(file,"");
 five.book(); five.format(); five.analyze(); five.save(); five.unbook();
-*/
+
 PerformanceOverlay figures(file);
 
 figures.book();
-figures.overlay("/TIME");
-figures.save("/TIME");
+figures.read("");
+figures.read("");
+figures.read("");
+figures.read("");
+figures.read("");
+figures.overlay();
 figures.unbook();
 
-figures.book();
-figures.overlay("/TIMING_VS_OCCUPANCY");
-figures.save("/TIMING_VS_OCCUPANCY");
-figures.unbook();
-
-figures.book();
-figures.overlay("/TIMING_VS_DIGIS");
-figures.save("/TIMING_VS_DIGIS");
-figures.unbook();
-
-figures.book();
-figures.overlay("/TIMING_VS_CLUSTERS");
-figures.save("/TIMING_VS_CLUSTERS");
-figures.unbook();
-
-figures.book();
-figures.overlay("/TIMING_VS_CLUSTERSIZE");
-figures.save("/TIMING_VS_CLUSTERSIZE");
-figures.unbook();
-
-figures.book();
-figures.overlay("/TIMING_VS_FRAC");
-figures.save("/TIMING_VS_FRAC");
-figures.unbook();
-/*
-figures.book();
-figures.overlay("/EFFICIENCY_HLT");
-figures.save("/EFFICIENCY_HLT");
-figures.unbook();
-
-figures.book();
-figures.overlay("/EFFICIENCY_VSPT_1");
-figures.save("/EFFICIENCY_VSPT_1");
-figures.unbook();
-
-figures.book();
-figures.overlay("/EFFICIENCY_VSPT_2");
-figures.save("/EFFICIENCY_VSPT_2");
-figures.unbook();
-
-figures.book();
-figures.overlay("/EFFICIENCY_VSETA_1");
-figures.save("/EFFICIENCY_VSETA_1");
-figures.unbook();
-
-figures.book();
-figures.overlay("/EFFICIENCY_VSETA_2");
-figures.save("/EFFICIENCY_VSETA_2");
-figures.unbook();
-
-figures.book();
-figures.overlay("/EFFICIENCY_VSETA_3");
-figures.save("/EFFICIENCY_VSETA_3");
-figures.unbook();
-*/
-new TBrowser;
-//file->Close();
+file->Close();
 
 }
