ab619368a1d87df64737d538db3d594479080f4e
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-09-11 11:17:04 +0000

    changes for new class names

--- test/stubs/macros/Build.cpp
+++ test/stubs/macros/Build.cpp
@@ -2,8 +2,8 @@
 gROOT->Reset();
 gSystem->AddIncludePath("-I/$CMSSW_BASE/src/");
 gSystem->Load("$CMSSW_BASE/lib/slc4_ia32_gcc345/libtestEventFilterSiStripRawToDigi.so");
-gSystem->CompileMacro("Plots.cc","fkg","libPlots");
 gSystem->CompileMacro("ObjectFilter.cc","fkg","libObjectFilter");
-gSystem->CompileMacro("PerformanceAnalysis.cc","fkg","libPerformanceAnalysis");
-gSystem->CompileMacro("PerformanceOverlay.cc","fkg","libPerformanceOverlay");
+gSystem->CompileMacro("SiStripPerformancePlots.cc","fkg","libSiStripPerformancePlots");
+gSystem->CompileMacro("SiStripPerformanceAnalysis.cc","fkg","libSiStripPerformanceAnalysis");
+gSystem->CompileMacro("SiStripPerformanceFigures.cc","fkg","libSiStripPerformanceFigures");
 }
