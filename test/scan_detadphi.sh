b028e9a6ad46ab2f0e8597d6dc7ef83a6ea849e9
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-10-19 14:26:42 +0000

    minor

--- test/scan_detadphi.sh
+++ test/scan_detadphi.sh
@@ -34,13 +34,17 @@ string='"'
 endline=' #'
 
 #run
-for delta in 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.12 0.14 0.16 0.18 0.2 0.22 0.24 0.26 0.28 0.3 
+for delta in 
+#TAU-JETS 0.0 0.02 0.04 0.06 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.18 0.2 0.22 0.24 0.26 0.28 0.3 0.32 0.34 0.36 0.38 0.4 0.42 0.44 0.46 0.48 0.5
+#B-JETS 0.0 0.02 0.04 0.06 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.18 0.2 0.22 0.24 0.26 0.28 0.3 0.32 0.34 0.36 0.38 0.4 0.42 0.44 0.46 0.48 0.5
+#LEPTONS 0.0 0.02 0.04 0.06 0.08 0.095 0.1 0.105 0.11 0.115 0.12 0.125 0.13 0.135 0.14 0.145 0.15 0.16 0.18 0.2 
 do
 name=$string$delta$string;
 value=$delta;
 replace "$etafield" "$etafield$equals$value$endline" -- $ROI;
 replace "$phifield" "$phifield$equals$value$endline" -- $ROI;
 replace "$namefield" "$namefield$equals$name$endline" -- $PERFORMANCE;
+echo "scan_detadphi.sh: Run setting " $name
 cmsRun $RUN;
 done
 
