e3fb394cacb688af0be4eed02c6b4c15c0be89e9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-06-06 21:08:42 +0000

    back out change made obsolete by EDAlias feature

--- python/SiStripDigiToRaw_cfi.py
+++ python/SiStripDigiToRaw_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 SiStripDigiToRaw = cms.EDProducer(
     "SiStripDigiToRawModule",
-    InputModuleLabel = cms.string('mix'),
+    InputModuleLabel = cms.string('simSiStripDigis'),
     InputDigiLabel = cms.string('ZeroSuppressed'),
     FedReadoutMode = cms.string('ZERO_SUPPRESSED'),
     UseFedKey = cms.bool(False),
